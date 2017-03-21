c44eabf82c8f8dc23e47f971732f24faabc090fb
ignacebertrand <ignacetakupo@gmail.com>
2017-03-21 14:56:39 -0400

    project add

---
+++ src/Sample.java
@@ -0,0 +1,12 @@
+
+public class Sample {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		String inFolder = "../Data/";
+		String outFolder = "../Sample/";
+		
+
+	}
+
+}
